7b861ed3ecbbbc7d9d4bbb5be30814d145e1d75c
Abdulrahman Alwakeel <b98576479@gmail.com>
2025-05-09 19:40:18 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+//Audio function
+document.addEventListener('click', function () {
+    const audio = document.getElementById('bg-audio');
+    audio.play();
+    });
+    //shr7 alfunction btsh8l autoplay llaudio
